33d405ff3b0f78d48bc523568de079ebefabfe3d
Andrey Gubarev <mylokin@me.com>
2015-04-11 18:24:33 +0100

    Revert "use pytest fixtures"
    This reverts commit 0a08a5eab8a5f3ce7e1b2c2e051a6c3395d6992c.

--- tests/test_client.py
+++ tests/test_client.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 import io
+import unittest
 import urllib2
 
 import mock
@@ -11,17 +12,7 @@ import servy.exc
 import servy.proto
 
 
-@pytest.fixture
-def service():
-    return servy.client.Request('localhost', 'serv')
-
-
-@pytest.fixture
-def client(service):
-    return servy.client.Client('localhost', service)
-
-
-class TestRequest(object):
+class Request(unittest.TestCase):
     def test_url(self):
         service = servy.client.Request('localhost', 'serv')
         assert service.url == 'http://localhost/serv'
@@ -39,42 +30,46 @@ class TestRequest(object):
         assert client._Client__service.url == 'http://localhost:80/serv'
 
 
-class TestRemoteExecution(object):
-    def test_remote_execution(self, client):
+class RemoteExecution(unittest.TestCase):
+    def setUp(self):
+        self.service = servy.client.Request('localhost', 'serv')
+        self.client = servy.client.Client('localhost', self.service)
+
+    def test_remote_execution(self):
         content = servy.proto.Response.encode('content')
         with mock.patch('servy.client.Request.read') as read:
             read.return_value = content
-            assert client.fn() == servy.proto.Response.decode(content)
+            assert self.client.fn() == servy.proto.Response.decode(content)
         message = servy.proto.Request.encode((), {})
         read.assert_called_once_with(message)
 
-    def test_failed_remote_execution(self, client):
+    def test_failed_remote_execution(self):
         with pytest.raises(TypeError):
-            client()
+            self.client()
 
-    def test_http_exception_404(self, client, service):
+    def test_http_exception_404(self):
         with mock.patch('servy.client.Request.read') as read:
-            read.side_effect = urllib2.HTTPError(service.url, 404, 'Not Found', [], io.StringIO())
+            read.side_effect = urllib2.HTTPError(self.service.url, 404, 'Not Found', [], io.StringIO())
             with pytest.raises(servy.exc.ServiceNotFound):
-                client.fn()
+                self.client.fn()
 
-    def test_http_exception_503(self, client, service):
+    def test_http_exception_503(self):
         with mock.patch('servy.client.Request.read') as read:
             fp = io.StringIO()
             fp.write(unicode(servy.proto.RemoteException.encode('traceback')))
             fp.seek(0)
-            read.side_effect = urllib2.HTTPError(service.url, 503, 'Service Unavailable', [], fp)
+            read.side_effect = urllib2.HTTPError(self.service.url, 503, 'Service Unavailable', [], fp)
             with pytest.raises(servy.exc.RemoteException):
-                client.fn()
+                self.client.fn()
 
-    def test_http_exception_503_failed(self, client, service):
+    def test_http_exception_503_failed(self):
         with mock.patch('servy.client.Request.read') as read:
-            read.side_effect = urllib2.HTTPError(service.url, 503, 'Service Unavailable', [], io.StringIO())
+            read.side_effect = urllib2.HTTPError(self.service.url, 503, 'Service Unavailable', [], io.StringIO())
             with pytest.raises(servy.exc.RemoteException):
-                client.fn()
+                self.client.fn()
 
-    def test_uncovered_http_exception(self, client, service):
+    def test_uncovered_http_exception(self):
         with mock.patch('servy.client.Request.read') as read:
-            read.side_effect = urllib2.HTTPError(service.url, 600, 'Magic', [], io.StringIO())
+            read.side_effect = urllib2.HTTPError(self.service.url, 600, 'Magic', [], io.StringIO())
             with pytest.raises(urllib2.HTTPError):
-                client.fn()
+                self.client.fn()
